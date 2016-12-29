08b9d4825a78580a46ce41dec224c19c9d91e4d0
Kiran Vishal Thanjavur Bhaaskar <vishal94@bu.edu>
2016-12-29 16:11:14 -0500

    Feature Extraction working code

--- SADDCtree-chisel/src/main/scala/FeatureExtractor.scala
+++ SADDCtree-chisel/src/main/scala/FeatureExtractor.scala
@@ -1,56 +1,133 @@
-package Chisel
+// package Chisel
 
+// import scala.collection.mutable.{ArrayBuffer, HashMap}
+// import java.io._
+// //template for FE chisel 
+//  def read_file(filename: String, n: Int): String = {
+//     val buffer = new StringBuilder
+    
+//     val n_node = UInt(0, width = 32)
+//     val leaf_node = UInt(0, width = 32)
+//     val nonleaf_node = UInt(0, width = 32)
+
+//     val current_node = Array.fill(n){UInt(0, width = 32)}  
+//     val left_node = Array.fill(n){UInt(0, width = 32)}
+//     val right_node = Array.fill(n){UInt(0, width = 32)} 
+//     val feature_index = Array.fill(n){UInt(0, width = 32)} 
+//     val threshold = Array.fill(n){UInt(0, width = 32)} 
+    
+//     val i = UInt(0, width = 32)
+    
+//     try {
+//       val reader = new BufferedReader(new FileReader(filename))
+//       var line = ""
+//       while({line = reader.readLine(); line != null}) {
+//         buffer.append(line)
+//         buffer.append("\n")
+//         val line_array =line.split(" ")
+//         when (i == 0){
+//            n_node = line_array[0]
+//            leaf_node = line_array[1]
+//            nonleaf_node = line_array[2]
+//            printf("%d %d %d \n", n_node, leaf_node, nonleaf_node)
+//         } 
+//         .elsewhen((i!=0)&&(i!=n)){
+//           current_node[i] = line_array[0]
+//           left_node[i] = line_array[1]
+//           right_node[i] = line_array[2]
+//           feature_index[i] = line_array[3]
+//           threshold[i] = line_array[4]
+//           printf("%d %d %d %d \n", current_node[i], left_node[i], right_node[i], feature_index[i], threshold[i])
+//         }
+//         i = i + 1
+//       }
+//       reader.close()
+//     } catch {
+//       case e: IOException => {
+//         System.err.println("Error reading file " + filename)
+//         System.exit(-1)
+//       }
+//     }
+//     buffer.toString()
+//   }
+
+
+//   // val myUInt= "hi hi"
+//   // val myU = myUInt.split(" ")
+//   // printf(myU(0))
+//   // printf(myU(1))
+
+package cont
 import scala.collection.mutable.{ArrayBuffer, HashMap}
 import java.io._
+//import Chisel._
 //template for FE chisel 
- def read_file(filename: String): String = {
-    val buffer = new StringBuilder
-    
-    val n_node = UInt(0)
-    val leaf_node = UInt(0)
-    val nonleaf_node = UInt(0)
 
-    val current_node[n] = 0  
 
-    val left_node[n] = 0
-    val right_node[n] = 0
-    val feature_used[n] = 0
-    val threshold[n] = 0
-    
-    val i = UInt(0)
-    
-    try {
-      val reader = new BufferedReader(new FileReader(./FE_iris.csv))
-      var line = ""
-      while({line = reader.readLine(); line != null}) {
-        buffer.append(line)
-        buffer.append("\n")
-        val line_array =line.split(" ")
-        when (i == 0){
-           n_node = line_array[0]
-           leaf_node = line_array[1]
-           nonleaf_node = line_array[2]
-        } 
-        .elsewhen((i!=0)&&(i!=n)){
-          current_node[i] = line_array[0]
-          left_node[i] = line_array[1]
-          right_node[i] = line_array[2]
-          feature_used[i] = line_array[3]
-          threshold[i] = line_array[4]
-        }
-        i = i + 1
-      }
-      reader.close()
-    } catch {
-      case e: IOException => {
-        System.err.println("Error reading file " + filename)
-        System.exit(-1)
-      }
-    }
-    buffer.toString()
+
+object cont {
+  def main(args: Array[String]): Unit = {
+  // chiselMainTest(args, () => Module(new ControlUnit, {case No_Comparator=> 10} )) {
+  //   c => new ControlUnitTests(c) 
+  printf("hiiiiiiii")
+  //cont.read_file("./FE_iris.csv")
+  //val line_arrayint1 = "1 2 3 4 5 6 7".split(" ").map(_.toInt).distinct
+  //printf("%d %d %d", line_arrayint1(0), line_arrayint1(2), line_arrayint1(4))
+  read_file("./FE_iris.csv")
   }
-
-
+ def read_file(filename: String): String = {
+  val buffer = new StringBuilder
+  
+  var n_node = 0
+  var leaf_node = 0
+  var nonleaf_node = 0
+  var i = 0
+  try{
+    
+    val reader = new BufferedReader(new FileReader(filename))
+    var line = ""
+    
+    line = reader.readLine()
+    buffer.append(line)
+    buffer.append("\n")
+    val line_arrayint = line.split(" ").map(_.toInt).distinct
+    //val line_arrayint = line_array.map(x => x.toInt)
+    n_node = line_arrayint(0)
+    leaf_node = line_arrayint(1)
+    nonleaf_node = line_arrayint(2)
+    i = i + 1
+    printf("%d %d %d \n", n_node, leaf_node, nonleaf_node)
+  
+    val current_node = Array.fill(n_node){0}  
+    val left_node = Array.fill(n_node){0}
+    val right_node = Array.fill(n_node){0} 
+    val feature_index = Array.fill(n_node){0} 
+    val threshold = Array.fill(n_node){0.0} 
+    i = 0
+    while({line = reader.readLine(); line != null}) {
+      buffer.append(line)
+      buffer.append("\n")
+      val line_array =line.split(" ")
+        current_node(i) = line_array(0).toInt
+        left_node(i) = line_array(1).toInt
+        right_node(i) = line_array(2).toInt
+        feature_index(i) = line_array(3).toInt
+        threshold(i) = line_array(4).toDouble
+        printf("%d \n", i  )
+        printf("%d %d %d %d \n", current_node(i), left_node(i), right_node(i), feature_index(i), threshold(i))
+      i = i + 1
+    }
+    reader.close()
+  }
+  catch {
+    case e: IOException => {
+      System.err.println("Error reading file 1 " + filename)
+      System.exit(-1)
+    }
+  }
+  buffer.toString()
+}
+ }
   // val myUInt= "hi hi"
   // val myU = myUInt.split(" ")
   // printf(myU(0))
